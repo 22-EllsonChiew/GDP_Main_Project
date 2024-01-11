37f986a6b26517c4971aeff2bd8dd68975bc9636
ItzUzume <ellson637@gmail.com>
2024-01-11 14:16:02 +0800

    small update menu

--- GDP_Home_Sweet_Home_Project/Assets/Script/SceneManagement.cs
+++ GDP_Home_Sweet_Home_Project/Assets/Script/SceneManagement.cs
@@ -14,10 +14,28 @@ public class SceneManagement : MonoBehaviour
         Button btn = replayButton.GetComponent<Button>();
 
         btn.onClick.AddListener(MainGameScene);
+
+       
+    }
+
+    private void Update()
+    {
+        
     }
 
     void MainGameScene()
     {
         SceneManager.LoadScene("SampleScene 1");
+    }
+
+    public  void MainMenu()
+    {
+        SceneManager.LoadScene("Instruction");
+    }
+
+    public void GameScene()
+    {
+        SceneManager.LoadScene("SampleScene 1");
+
     }
 }
