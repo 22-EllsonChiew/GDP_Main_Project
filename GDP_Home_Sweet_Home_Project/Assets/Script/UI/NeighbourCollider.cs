aa01b0838fb79fe2362ac2082902f2b3594151ee
sirpotatosalad <timothy.a.anoso@gmail.com>
2024-07-14 19:21:26 +0800

    Started UI integration

---
+++ GDP_Home_Sweet_Home_Project/Assets/Script/UI/NeighbourCollider.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeighbourCollider : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject neighbourUIGroup;
+
+    private bool playerCanInteract;
+    void Start()
+    {
+        neighbourUIGroup.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerCanInteract && Input.GetKeyUp(KeyCode.E)) 
+        {
+            neighbourUIGroup.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCanInteract = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCanInteract = false;
+            if (neighbourUIGroup.activeSelf)
+            {
+                neighbourUIGroup.SetActive(false);
+            }
+        }
+    }
+}
