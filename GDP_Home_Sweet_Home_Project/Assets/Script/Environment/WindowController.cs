c72d90ee5f952f2367569a63f3db6846a44c5975
OrekiPotato <mojangthesimlord@outlook.com>
2024-07-16 16:19:29 +0800

    Commit@ 16/7/2024
    Imported Window asset with close animation

---
+++ GDP_Home_Sweet_Home_Project/Assets/Script/Environment/WindowController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class WindowController : MonoBehaviour
+{
+    private Animator animator;
+    private bool isOpen = true;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O)) // Replace with your preferred key
+        {
+            TogglePanels();
+        }
+    }
+
+    public void TogglePanels()
+    {
+        if (isOpen)
+        {
+            animator.SetTrigger("CloseAllPanels"); // Trigger for closing all panels
+            isOpen = false;
+        }
+        else
+        {
+            animator.SetTrigger("OpenAllPanels"); // Trigger for opening all panels
+            isOpen = true;
+        }
+    }
+}
